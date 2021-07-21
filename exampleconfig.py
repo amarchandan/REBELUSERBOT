6f4b716927ccc16702eafcc2a5dacb5770d284ea
モメRÈBÉL我 <78189163+REBEL75@users.noreply.github.com>
2021-07-21 20:22:29 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -8,7 +8,7 @@ class Development(Config):
   API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
 
   # the name to display in your alive message.
-  # If not filled anything then default value is REBEL User.
+  # If not filled anything then default value is Mafia User.
   ALIVE_NAME = "REBEL User"
 
   # create any PostgreSQL database.
