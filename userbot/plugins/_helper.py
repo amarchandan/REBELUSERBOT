8b53b9c8b11c761323d6b1f55da7bb75a9151cb8
モメRÈBÉL我 <78189163+REBEL75@users.noreply.github.com>
2021-07-25 17:16:53 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -13,19 +13,23 @@ from REBELBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 async def yardim(event):
     if event.fwd_from:
         return
-    tgbotusername = Config.BOT_USERNAME
+    tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
     input_str = event.pattern_match.group(1)
-    try:
-        if not input_str == "":
-            if input_str in CMD_HELP:
-                await eor(event, str(CMD_HELP[args]))
-    except:
-        pass
-    if tgbotusername is not None:
-        results = await event.client.inline_query(tgbotusername, "REBELBOT_help")
+    if tgbotusername is not None or REBEL_input == "text":
+        results = await event.client.inline_query(tgbotusername, "@REBELBOT_SUPPORT")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
         await event.delete()
     else:
-        await eor(event, "**⚠️ ERROR !!** \nPlease Re-Check BOT_TOKEN & BOT_USERNAME on Heroku.")
+        await edit_or_reply(event, ["NO_BOT"])
+    
+        if input_str in CMD_LIST:
+          string = "Commands found in {}:\n".format(input_str)
+          for i in CMD_LIST[input_str]:
+              string += "  " + i
+              string += "\n"
+          await event.edit(string)
+        else:
+          await event.edit(input_str + " is not a valid plugin!")
+
