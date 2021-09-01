09c66ccdd0018a9d7c3ff4e01a8a8301c0102378
モメRÈBÉL我 <78189163+REBEL75@users.noreply.github.com>
2021-09-01 14:18:02 +0530

    Update plugin_info.py

--- userbot/plugins/plugin_info.py
+++ userbot/plugins/plugin_info.py
@@ -10,10 +10,10 @@ from REBELBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 
-@bot.on(admin_cmd(pattern=r"plinfo(?: |$)(.*)"))
-@bot.on(sudo_cmd(pattern=r"plinfo(?: |$)(.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern=r"help(?: |$)(.*)"))
+@bot.on(sudo_cmd(pattern=r"help(?: |$)(.*)", allow_sudo=True))
 async def info(event):
-    """ For .plinfo command,"""
+    """ For .help command,"""
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CmdHelp:
@@ -22,7 +22,7 @@ async def info(event):
             await edit_or_reply(event, "Please specify a valid plugin name.")
     else:
         await edit_or_reply(event, "Please specify which plugin do you want help for !!\
-            \nUsage: .plinfo <plugin name>"
+            \nUsage: .help <plugin name>"
         )
         string = ""
         for i in CmdHelp:
@@ -31,5 +31,5 @@ async def info(event):
         await event.reply(string)
 
 CmdHelp("plugin_info").add_command(
-  "plinfo", "<plugin name>", "Gives the info triggered plugin. Every Commands with its usage and how to use.."
+  "help", "<plugin name>", "Gives the info triggered plugin. Every Commands with its usage and how to use.."
 ).add()
