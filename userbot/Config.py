9278fba993fc6530c3d74496e33b5bfa651e3428
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2021-09-21 15:39:31 +0530

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -149,7 +149,7 @@ if ENV:
         LYDIA_API = os.environ.get("LYDIA_API",None)
         PLUGIN_CHANNEL = int(os.environ.get("PLUGIN_CHANNEL", None))
         UPSTREAM_REPO = os.environ.get(
-            "UPSTREAM_REPO", "https://github.com/REBEL75/REBELBOT"
+            "UPSTREAM_REPO", "https://github.com/REBEL75/REBELBOTOP"
         )
         STRING_SESSION = os.environ.get("STRING_SESSION", None)
         BOT_MODE = os.environ.get("BOT_MODE", "ON")
