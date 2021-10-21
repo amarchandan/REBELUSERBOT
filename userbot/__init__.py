c9bdb1cfa0caee95f74d25625a99a61aed8729f0
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2021-10-21 14:34:44 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -149,7 +149,7 @@ except:
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/REBEL75/REBELBOTOP.git")
+    "https://github.com/REBEL75/REBELSBOTS.git")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
