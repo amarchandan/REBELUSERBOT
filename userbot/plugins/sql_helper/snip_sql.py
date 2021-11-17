6bc4731d3c10c071917d8e52dde030bc307c2eb9
REBELBOT <78189163+REBEL75@users.noreply.github.com>
2021-11-18 00:31:46 +0530

    Update snip_sql.py

--- userbot/plugins/sql_helper/snip_sql.py
+++ userbot/plugins/sql_helper/snip_sql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, LargeBinary, Numeric, UnicodeText
 
-from mafiabot.plugins.sql_helper import BASE, SESSION
+from REBELBOT.plugins.sql_helper import BASE, SESSION
 
 
 class Snips(BASE):
